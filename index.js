97bd70438c68c67e394a66338e6a2471b07ab142
kamnianjkajposla <sigam11k@wp.pl>
2026-09-22 16:08:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.listen(PORT, () => {
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: 'blokskraft.aternos.me:',        // <-- TUTAJ wpisz adres serwera (np. mojserwer.aternos.me)
+    host: 'blokskraft.aternos.me',        // <-- TUTAJ wpisz adres serwera (np. mojserwer.aternos.me)
     port: 50703,                         // <-- TUTAJ wpisz port serwera (zazwyczaj 25565)
     username: 'jaandzejmisi',        // <-- TUTAJ wpisz nick bota w grze
     version: false                       // false = automatyczna detekcja wersji
